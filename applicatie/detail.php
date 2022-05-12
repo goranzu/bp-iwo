f3a778741f53a22f09ad23cac2b6c90fa01ce31f
Goran Tenic <tenic.g@gmail.com>
2022-05-12 20:46:10 +0200

    detail page

---
+++ applicatie/detail.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Detail Page</title>
+</head>
+
+<body>
+    <h1 class="fs-xl">Batman Pagina</h1>
+</body>
+
+</html>
