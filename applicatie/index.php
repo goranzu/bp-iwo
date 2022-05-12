a341fbb1febde4efb889e364aaf6e19f0a07a079
Goran Tenic <tenic.g@gmail.com>
2022-05-12 14:15:52 +0200

    wip:  navigation

--- applicatie/index.php
+++ applicatie/index.php
@@ -26,6 +26,15 @@
                     <li><a href="/register.php">Register</a></li>
                     <li><a href="/about.php">About Us</a></li>
                 </ul>
+                <button class="menu-btn" aria-labelledby="menu-btn-label">
+                    <span id="menu-btn-label" class="sr-only">
+                        Open the mobile navigation menu.
+                    </span>
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                        <line x1="3" y1="12" x2="21" y2="12"></line>
+                        <line x1="3" y1="6" x2="21" y2="6"></line>
+                        <line x1="3" y1="18" x2="21" y2="18"></line>
+                    </svg> </button>
             </nav>
         </header>
 
